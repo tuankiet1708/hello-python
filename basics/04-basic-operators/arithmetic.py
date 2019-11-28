c2696d98f94bfcf78412c4306f32fd9aaedf973d
Kiet Tran <tuankiet1708@gmail.com>
2019-11-28 19:44:52 +0700

    Basic Operators

---
+++ basics/04-basic-operators/arithmetic.py
@@ -0,0 +1,14 @@
+# Arithmetic Operators
+# Just as any other programming languages, the addition, subtraction, multiplication, and division operators can be used with numbers.
+number = 1 + 2 * 3 / 4.0
+print(number)
+
+# Another operator available is the modulo (%) operator, which returns the integer remainder of the division. dividend % divisor = remainder.
+remainder = 11 % 3
+print(remainder)
+
+# Using two multiplication symbols makes a power relationship.
+squared = 7 ** 2
+cubed = 2 ** 3
+print(squared)
+print(cubed)
---
+++ basics/04-basic-operators/lists.py
@@ -0,0 +1,9 @@
+# Using Operators with Lists
+# Lists can be joined with the addition operators:
+even_numbers = [2,4,6,8]
+odd_numbers = [1,3,5,7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
+
+# Just as in strings, Python supports forming new lists with a repeating sequence using the multiplication operator:
+print([1,2,3] * 3)
