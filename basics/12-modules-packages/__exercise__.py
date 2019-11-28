be95e4ae00ddfd84b3cabb098b0c7c9723e32748
Kiet Tran <tuankiet1708@gmail.com>
2019-11-28 22:06:29 +0700

    Modules and Packages

---
+++ basics/12-modules-packages/__exercise__.py
@@ -0,0 +1,6 @@
+# Exercise
+# In this exercise, you will need to print an alphabetically sorted list of all functions in the re module, which contain the word find.
+
+import re
+
+# Your code goes here
