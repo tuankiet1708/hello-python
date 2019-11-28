6c2e8e66af4b55f04099879adac52dcb68549f3d
Kiet Tran <tuankiet1708@gmail.com>
2019-11-28 21:34:07 +0700

    Dictionaries

---
+++ basics/11-dictionaries/__solution__.py
@@ -0,0 +1,20 @@
+# Exercise
+# Add "Jake" to the phonebook with the phone number 938273443, and remove Jill from the phonebook.
+phonebook = {
+    "John" : 938477566,
+    "Jack" : 938377264,
+    "Jill" : 947662781
+}
+
+# write your code here
+phonebook["Jake"] = 938273443
+phonebook.pop("Jill")
+
+if "Jill" in phonebook:
+    del phonebook["Jill"]
+
+# testing code
+if "Jake" in phonebook:
+    print("Jake is listed in the phonebook.")
+if "Jill" not in phonebook:
+    print("Jill is not listed in the phonebook.")
