a8ce0109b77b30f98ba02d8f2945378e033d31d5
Kiet Tran <tuankiet1708@gmail.com>
2019-12-07 17:11:32 +0700

    Partial Functions

---
+++ advanced/07-partial-functions/__solution__.py
@@ -0,0 +1,10 @@
+# Edit the function provided by calling partial() and replacing the first three variables in func(). Then print with the new partial function using only one input variable so that the output equals 60.
+
+#Following is the exercise, function provided:
+from functools import partial
+def func(u,v,w,x):
+    return u*4 + v*3 + w*2 + x
+#Enter your code here to create and print with your partial function
+
+newfunc = partial(func,7,6,5)
+print(newfunc(4))
