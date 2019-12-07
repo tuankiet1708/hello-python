db7b78dbe7e8ff2d1e3a0e58016133f9455e1726
Kiet Tran <tuankiet1708@gmail.com>
2019-12-07 17:44:36 +0700

    Closures

---
+++ advanced/09-closures/__solution__.py
@@ -0,0 +1,10 @@
+# Make a nested loop and a python closure to make functions to get multiple multiplication functions using closures. That is using closures, one could make functions to create multiply_with_5() or multiply_with_4() functions using closures.
+
+# your code goes here
+def multiplier_of(number):
+    def multiplier(withnumber):
+        return withnumber * number
+    return multiplier
+
+multiplywith5 = multiplier_of(5)
+print(multiplywith5(9))
